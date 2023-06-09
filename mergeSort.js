bf2cff749ce3e26259326e4018ea991aaa3debce
shilagha0612 <sshilagha786@gmail.com>
2023-06-09 13:56:32 -0400

    MergeSort

---
+++ mergeSort.js
@@ -0,0 +1,126 @@
+/* 
+  Stable sort.
+  Visualization:
+  https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+  Time Complexity
+    - Best case: O(n log(n)) linearithmic.
+    - Average case: O(n log(n)) linearithmic.
+    - Worst case: O(n log(n)) linearithmic.
+  Space: O(n) linear
+  steps:
+    1. create a merge function to merge two already sorted arrays into a single
+        sorted array.
+      - you do NOT need to work in place, ok to use a new array
+    2. create mergeSort function to sort the provided array
+      - split the array in half and recursively merge the halves using the
+          previously created merge function.
+      - splitting of arrays stops when array can no longer be split.
+      - an array of 1 item is by definition sorted, so two arrays of 1 item
+          can therefore be merged into a sorted array.
+*/
+
+// merge
+const sortedA1 = [];
+const sortedB1 = [];
+const expectedMerge1 = [];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [3];
+const sortedB3 = [2, 3, 4, 7];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10];
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Efficiently merges two already sorted arrays into a new sorted array.
+ * Do not mutate the given arrays.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ */
+function merge(left, right) {
+    const merged = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] <= right[rightIndex]) {
+            merged.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            merged.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    // Add any remaining elements from the left and right arrays
+    while (leftIndex < left.length) {
+        merged.push(left[leftIndex]);
+        leftIndex++;
+    }
+
+    while (rightIndex < right.length) {
+        merged.push(right[rightIndex]);
+        rightIndex++;
+    }
+
+    return merged;
+}
+
+
+
+// Example usage:
+console.log(merge(sortedA1, sortedB1)); // Output: []
+console.log(merge(sortedA2, sortedB2)); // Output: [2, 5]
+console.log(merge(sortedA3, sortedB3)); // Output: [2, 3, 3, 4, 7]
+console.log(merge(sortedA4, sortedB4)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5,]
+
+// console.log(merge(sortedA1, sortedB1));
+// console.log(merge(sortedA2, sortedB2));
+// console.log(merge(sortedA3, sortedB3));
+// console.log(merge(sortedA4, sortedB4));
+
+// // mergeSort
+// const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const expectedSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Creates a new sorted array based on the given nums being recursively split
+ * and merged.
+ * Best: O(n log(n)) linearithmic.
+ * Avg: O(n log(n)) linearithmic.
+ * Worst: O(n log(n)) linearithmic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} A New sorted array.
+ */
+function mergeSort(nums) {
+    if (nums.length <= 1) {
+        return nums;
+    }
+
+    const mid = Math.floor(nums.length / 2);
+    const left = mergeSort(nums.slice(0, mid));
+    const right = mergeSort(nums.slice(mid));
+
+    return merge(left, right);
+}
+const ex = [10, 8, 6, 4, 3, 7, 9, 1, 2, 5];
+console.log(mergeSort(ex))
+
+/*****************************************************************************/
+
+module.exports = { merge, mergeSort };
